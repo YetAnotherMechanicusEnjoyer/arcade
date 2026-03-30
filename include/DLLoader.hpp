cce97e9b495350a02b3e9cf9ae95f47b681e1f30
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-03-30 09:32:40 +0200

    [ADD] DLLoader

---
+++ include/DLLoader.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <string>
+#include <memory>
+#include <dlfcn.h>
+#include "Errors.hpp"
+
+namespace Arcade {
+template <typename T>
+
+class DLLoader {
+private:
+  void *_handle;
+
+public:
+  explicit DLLoader(const std::string& filepath) : _handle(nullptr) {
+    _handle = dlopen(filepath.c_str(), RTLD_LAZY);
+    if (!_handle)
+      throw ARCError(dlerror());
+  }
+
+  ~DLLoader() {
+    if (_handle)
+      dlclose(_handle);
+  }
+
+  DLLoader(const DLLoader&) = delete;
+  DLLoader& operator=(const DLLoader&) = delete;
+
+  DLLoader(DLLoader&& other) noexcept : _handle(other._handle) {
+    other._handle = nullptr;
+  }
+
+  DLLoader& operator=(DLLoader&& other) noexcept {
+    if (this != &other) {
+      if (_handle)
+        dlclose(_handle);
+      _handle = other._handle = nullptr;
+    }
+    return *this;
+  }
+
+  std::unique_ptr<T> getInstance(const std::string& entryPointName = "entryPoint") {
+    dlerror();
+
+    using EntryPointFunc = T* (*)();
+    void* sym = dlsym(_handle, entryPointName.c_str());
+
+    const char *dlsym_error = dlerror();
+    if (dlsym_error)
+      throw ARCError(std::string("dlsym error : ") + dlsym_error);
+
+    EntryPointFunc createFunc = reinterpret_cast<EntryPointFunc>(sym);
+
+    return std::unique_ptr<T>(createFunc());
+  }
+};
+}
