8fdea34b69ae46d56c6ae814c8572c21afff8144
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-04-05 10:01:47 +0200

    [ADD] DirectoryScanner class

---
+++ include/DirectoryScanner.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <filesystem>
+#include "Errors.hpp"
+#include "DLLoader.hpp"
+
+namespace Arcade {
+class DirectoryScanner {
+public:
+  static void scan(const std::string& path, std::vector<std::string>& gameLibs, std::vector<std::string>& graphicLibs) {
+    gameLibs.clear();
+    graphicLibs.clear();
+
+    if (!std::filesystem::exists(path) || !std::filesystem::is_directory(path))
+      return;
+
+    for (const auto& entry : std::filesystem::directory_iterator(path)) {
+      if (entry.path().extension() == ".so") {
+        std::string filepath = entry.path().string();
+        try {
+          DLLoader<void> inspector(filepath);
+
+          if (inspector.hasSymbol("createGame")) {
+            gameLibs.push_back(filepath);
+          } else if (inspector.hasSymbol("createGraphics")) {
+            graphicLibs.push_back(filepath);
+          }
+        } catch (const ARCError&) {
+
+        }
+      }
+    }
+  }
+};
+}
